1335f2bc4bf0ae4294c5424891533c162666782f
Zumbi Azul <capoeira_basics@mail.ru>
2013-04-11 13:44:38 +0400

    Created a new project in Eclipse

---
+++ src/com/flexymind/hw04alarm/Flexymind_hw_04_alarmActivity.java
@@ -0,0 +1,13 @@
+package com.flexymind.hw04alarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Flexymind_hw_04_alarmActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
