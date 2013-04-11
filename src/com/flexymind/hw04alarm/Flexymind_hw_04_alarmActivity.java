aadc0bcf47b86c157d6d5a32c962652210d3bb5d
Zumbi Azul <capoeira_basics@mail.ru>
2013-04-11 15:26:05 +0400

    Modified main layout (ToggleButton added) and added ToggleButton onClick dummy

--- src/com/flexymind/hw04alarm/Flexymind_hw_04_alarmActivity.java
+++ src/com/flexymind/hw04alarm/Flexymind_hw_04_alarmActivity.java
@@ -2,12 +2,37 @@ package com.flexymind.hw04alarm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 public class Flexymind_hw_04_alarmActivity extends Activity {
+	
+	TimePicker timePckr;
+	ToggleButton tglBtn;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        timePckr = (TimePicker)findViewById(R.id.timePicker1);
+        tglBtn = (ToggleButton)findViewById(R.id.toggleButton1);
+        timePckr.setCurrentHour(9);
+        timePckr.setCurrentMinute(0);
+    }
+    
+    public void onToggleClicked(View view) {
+        // Is the toggle on?
+        boolean on = ((ToggleButton) view).isChecked();
+        
+        if (on) {
+        	timePckr.setCurrentHour(15);
+            timePckr.setCurrentMinute(20);
+        } else {
+        	timePckr.setCurrentHour(9);
+            timePckr.setCurrentMinute(0);
+        }
     }
 }
