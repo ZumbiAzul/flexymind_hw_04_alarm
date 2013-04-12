c070fa9618974804f06a253eb877b90d38535c37
Zumbi Azul <capoeira_basics@mail.ru>
2013-04-12 14:45:16 +0400

    Added dummies for services and buttons of second activity.

--- src/com/flexymind/hw04alarm/Alarm.java
+++ src/com/flexymind/hw04alarm/Alarm.java
@@ -1,12 +1,29 @@
 package com.flexymind.hw04alarm;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Alarm extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.alarm);
+	    
+	    Intent intent = new Intent(this, AudioService.class);
+		startService(intent);
 	  }
+	
+	public void onSnooze(View view) {
+		Intent intent = new Intent(this, AudioService.class);
+		stopService(intent);
+		finish();
+	}
+	
+	public void onAlarmOff(View view) {
+		Intent intent = new Intent(this, AudioService.class);
+		stopService(intent);
+		finish();
+	}
 
 }
---
+++ src/com/flexymind/hw04alarm/AlarmService.java
@@ -0,0 +1,33 @@
+package com.flexymind.hw04alarm;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class AlarmService extends Service {
+	
+	public void onCreate() {
+		super.onCreate();
+	}
+	
+	public int onStartCommand(Intent intent, int flags, int startID) {
+		someTask();
+		return super.onStartCommand(intent, flags, startID);
+	}
+	
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	void someTask() {
+		Toast.makeText(getApplicationContext(), "AlarmService created", Toast.LENGTH_LONG).show();
+	}
+
+}
